abe4f0e7726a403a5bb5b0d3cf9cc3d264e92e14
Peter Molchanov <pmolchanov@gmail.com>
2016-03-03 23:35:14 -0800

    Removed adb unsecure dependency, jQuery dependency and other misc tweaks.

--- extract.sh
+++ extract.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-mkdir -p ./data && adb pull /data/data/com.google.android.apps.authenticator2/databases/databases && mv databases data/databases
+mkdir -p ./data && adb shell "su -c 'mkdir -p /sdcard/98862587-3283-4c73-91b9-1d3cac192673; cp /data/data/com.google.android.apps.authenticator2/databases/databases /sdcard/98862587-3283-4c73-91b9-1d3cac192673;'" && adb pull /sdcard/98862587-3283-4c73-91b9-1d3cac192673/databases ./data && adb shell "su -c 'rm -r /sdcard/98862587-3283-4c73-91b9-1d3cac192673'"
+
+
--- main.sh
+++ main.sh
@@ -3,5 +3,6 @@
 ./clean.sh
 ./extract.sh
 ./dumpcsv.sh
+sleep 1 && sensible-browser http://localhost:2001
 ./server.sh
 
